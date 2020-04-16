f04ea29f8f3adb4aa4dc5bd88eab23033a2202ac
Junhyung Ki <kijoonh91@gmail.com>
2020-04-17 01:05:32 +0200

    changes

--- src/random_walk/rw_visual.py
+++ src/random_walk/rw_visual.py
@@ -4,11 +4,11 @@ from random_walk import RandomWalk
 
 # Keep making new walks, as long as the program is active.
 while True:
-    # Make a random walk.
-    rw = RandomWalk(50_000)
+    # 1. Make a random walk.
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
-    # Plot the points in the walk.
+    # 2. Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     #fig, ax = plt.subplots(figsize=(15, 9))
@@ -16,13 +16,14 @@ while True:
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
         edgecolors='none', s=1)
+    #ax.plot(rw.x_values, rw.y_values, linewidth=1, zorder=1)
 
-    # Emphasize the first and last points.
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    # 3. Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100, zorder=2)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', 
-        s=100)
+        s=100, zorder=2)
 
-    # Remove the axes.
+    # 4. Remove the axes.
     #ax.get_xaxis().set_visible(False)
     #ax.get_yaxis().set_visible(False)
 
