794701978d9292a6898f10e54f26b1e45cdab145
Junhyung Ki <kijoonh91@gmail.com>
2020-04-17 21:37:12 +0200

    chages

--- src/rolling_dice/dice_visual.py
+++ src/rolling_dice/dice_visual.py
@@ -6,12 +6,14 @@ from die import Die
 # Create a D6 and a D10.
 die_1 = Die()
 die_2 = Die(10)
+# die_1, die_2 = Die(), Die()
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
+# results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
 
 # Analyze the rsults.
 frequencies = []
@@ -19,6 +21,8 @@ max_result = die_1.num_sides + die_2.num_sides
 for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+# max_result = die_1.num_sides + die_2.num_sides
+# frequencies = [results.count(value) for value in range(2, max_result+1)]
 
 # Visualize the results.
 x_values = list(range(2, max_result+1))
